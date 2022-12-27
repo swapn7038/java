461c6af46303fe238abee749a01de6b8d54c3f13
Swapnil Shelke <swapnilbabanshelke@gmail.com>
2022-12-27 12:12:26 +0530

    java...

--- GetDiscount.java
+++ GetDiscount.java
@@ -1,19 +1,31 @@
-// A shop will give discount of 10% if the cost of purchased quantity is more than 1000. Ask user for quantity 
-
-// Suppose, one unit will cost 100. 
-// Judge and print total cost for user.
+// A school has following rules for grading system:
+// a. Below 25 - F
+// b. 25 to 45 - E
+// c. 45 to 50 - D
+// d. 50 to 60 - C
+// e. 60 to 80 - B
+// f. Above 80 - A
+// Ask user to enter marks and print the corresponding grade.
 
 import java.util.Scanner;
-class GetDiscount {
+
+class Ans {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("enter quantity");
-        
+        System.out.println("Enter your marks : ");
         int x = s.nextInt();
-        if((x*100) > 1000){
-            System.out.println("you get a discount of " + (.1 * 100) + "and your total cost is " + (x*100 - (.1*100)));
-        } else {
-            System.out.println("No discount");
+        if(x < 25){
+            System.out.println("F");
         }
-    }
-}
+        else if ((x>=25) && (x<45)){
+            System.out.println("E");
+        }
+        else if ((x>=45) && (x<50)){
+            System.out.println("D");
+        }
+        else if ((x>=50) && (x<60)){
+            System.out.println("C");
+        }
+        else if ((x>=60) && (x<80)){
+            System.out.println("B");
+        }
