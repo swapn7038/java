48af26600dcd6b1fe07ec98ae98c5fb3d1c2426d
Swapnil Shelke <82700394+swapn7038@users.noreply.github.com>
2023-01-18 09:44:23 +0530

    Add files via upload

---
+++ JavaProgramsClg/oddEven/oddEven.java
@@ -0,0 +1,20 @@
+class oddEven {
+ public static void main(String[] args) {
+
+ int n = 4;
+
+ if(n % 2 == 1){
+  System.out.println("Number is odd");
+} else {
+
+   System.out.println("Number is even");
+}
+
+}
+  }   
+
+// Output 1 
+// Number is odd
+
+// Output 2 
+// Number is even
